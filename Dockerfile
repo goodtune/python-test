641f5b5ecb8554c7dd27b10999f668aa56f0a619
Gary Reynolds <gary@touch.asn.au>
2016-08-28 15:32:45 +1000

    Update container with utility command
    - run tests with tox
    - if we're not on master branch, bump the version to get a dev release
    - upload to devpi, along with tox results json file

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,15 @@ FROM python:3
 MAINTAINER Gary Reynolds <gary@touch.asn.au>
 
 RUN curl -s https://bootstrap.pypa.io/get-pip.py | python2.7
-RUN pip2.7 install --no-cache-dir tox devpi-client
+RUN pip2.7 install --no-cache-dir tox devpi-client bumpversion click
 
 RUN apt-get update && apt-get install -y \
   libpython2.7-dev \
   && rm -rf /var/lib/apt/lists/*
 
-RUN pip2.7 install bumpversion
+COPY run-tests.py /usr/local/bin/run-tests
 
 VOLUME /src
 WORKDIR /src
 
-ENTRYPOINT ["tox"]
+ENTRYPOINT ["run-tests"]
---
+++ run-tests.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2.7
+
+import click
+import subprocess
+import sys
+
+
+@click.command()
+@click.option('--result-json', default='result.json')
+@click.argument('devpi_endpoint')
+@click.option('--devpi-username', envvar='DEVPI_USERNAME')
+@click.option('--devpi-password', envvar='DEVPI_USERNAME')
+@click.option('--bitbucket-branch', envvar='BITBUCKET_BRANCH')
+def run(result_json, devpi_endpoint, devpi_username, devpi_password):
+    try:
+        subprocess.check_call(["tox", "-r", "--result-json", result_json])
+    except subprocess.CalledProcessError:
+        sys.exit(1)
+
+    try:
+        subprocess.check_call(["devpi", "use", devpi_endpoint])
+    except subprocess.CalledProcessError:
+        sys.exit(2)
+
+    try:
+        subprocess.check_call([
+            "devpi", "login", devpi_username, "--password", devpi_password])
+    except subprocess.CalledProcessError:
+        sys.exit(3)
+
+    if bitbucket_branch != 'master':
+        try:
+            subprocess.check_call([
+                "bumpversion", "dev",
+                "--list",
+                "--no-commit",
+                "--new-version=$(python setup.py --version).dev$(date +%s)"],
+                shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit(4)
+
+    try:
+        subprocess.check_call(["devpi", "upload", "--no-vcs"])
+    except subprocess.CalledProcessError:
+        sys.exit(5)
+
+    try:
+        name, version = subprocess.check_output([
+            sys.executable, "setup.py", "--name", "--version"]).split()
+    except subprocess.CalledProcessError:
+        sys.exit(6)
+
+    try:
+        devpi_list = subprocess.check_output([
+            "devpi", "list", name + b"==" + version]).split()[0]
+    except subprocess.CalledProcessError:
+        sys.exit(7)
+
+    try:
+        subprocess.check_call([
+            "curl", "-X", "POST", "--data-binary", "@result.json", devpi_list])
+    except subprocess.CalledProcessError:
+        sys.exit(8)
+
+
+if __name__ == '__main__':
+    run()
